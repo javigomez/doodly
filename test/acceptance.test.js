b3e6da99b20f0b57c401b8aa88d668b2a2a94c7c
Jorge Velasco <jorge@typeform.com>
2018-02-02 13:12:03 +0100

    Add test to describe app

--- test/acceptance.test.js
+++ test/acceptance.test.js
@@ -1,1 +1,9 @@
+describe('a doodle like app', () => {
+  describe('the creation of an event', () => {
+    it('gives an URL to share with attendees', () => {
+    })
 
+    it('gives an URL to administrate the event', () => {
+    })
+  })
+})
